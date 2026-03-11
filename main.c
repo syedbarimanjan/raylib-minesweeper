b7ce74d15b66178dde29006162f40f957a77be8a
syedbarimanjan <syedbarimanjan@gmail.com>
2026-03-11 18:15:56 +0500

    add functionality for showing mines number in boxes

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ typedef struct Cell {
   int j;
   bool containsMine;
   bool revealed;
+  int nearbyMines;
 } Cell;
 
 Cell grid[COLS][ROWS];
@@ -25,6 +26,7 @@ Cell grid[COLS][ROWS];
 void CellDraw(Cell);
 bool IndexIsValid(int, int);
 void CellReveal(int, int);
+int CellCountMines(int,int);
 
 int main() {
   srand(time(0));
@@ -32,7 +34,32 @@ int main() {
   
   for (int i = 0; i < COLS; i++) {
     for (int j = 0; j < ROWS; j++) {
-      grid[i][j] = (Cell) {.i = i, .j = j};
+      grid[i][j] = (Cell) {
+        .i = i,
+        .j = j,
+        .containsMine = false,
+        .revealed = false,
+        .nearbyMines = -1
+      };
+    }
+  }
+
+  int minesToPlace = (int)(ROWS * COLS * 0.1f);
+  while(minesToPlace > 0) {
+    int i = rand() % COLS;
+    int j = rand() % ROWS;
+
+    if(!grid[i][j].containsMine) {
+      grid[i][j].containsMine = true;
+      minesToPlace--;
+    }
+  }
+
+  for (int i = 0; i < COLS; i++) {
+    for (int j = 0; j < ROWS; j++) {
+      if(!grid[i][j].containsMine){
+        grid[i][j].nearbyMines = CellCountMines(i,j);
+      }
     }
   }
 
@@ -66,10 +93,13 @@ int main() {
 void CellDraw(Cell cell) {
   if(cell.revealed){
     if(cell.containsMine){
-      DrawRectangleLines( cell.i * cellWidth, cell.j *cellHeight, cellWidth, cellHeight, RED);
+      DrawRectangle( cell.i * cellWidth, cell.j *cellHeight, cellWidth, cellHeight, RED);
     } 
     else{
-      DrawRectangleLines( cell.i * cellWidth, cell.j *cellHeight, cellWidth, cellHeight, RED);
+      DrawRectangle( cell.i * cellWidth, cell.j *cellHeight, cellWidth, cellHeight, LIGHTGRAY);
+      if(cell.nearbyMines > 0) {
+        DrawText(TextFormat("%d", cell.nearbyMines),cell.i * cellWidth + 18,cell.j * cellHeight + 4, cellHeight - 8, DARKGRAY);
+      }
     }
   }
   DrawRectangleLines( cell.i * cellWidth, cell.j *cellHeight, cellWidth, cellHeight, BLACK);
@@ -88,4 +118,26 @@ void CellReveal(int i, int j) {
   else {
     //play sound
   }
+}
+
+int CellCountMines(int i, int j){
+  int count = 0;
+  for (int iOff = -1; iOff <= 1; iOff++) {
+    for (int jOff = -1; jOff <= 1; jOff++) {
+
+      if (iOff == 0 && jOff == 0) {
+        continue;
+      }
+
+      if(!IndexIsValid(i + iOff, j + jOff)){
+        continue;
+      }
+
+      if (grid[i + iOff][j + jOff].containsMine) {
+        count++;
+      }
+    }
+  }
+
+    return count;
 }
